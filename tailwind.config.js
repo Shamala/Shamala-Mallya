d20f998ffb9598bc5b0f1e06a0096e063aa48e42
Shamala <msurabhi23@gmail.com>
2023-03-08 12:48:39 -0800

    Simplified website

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         lato: ["Lato", "sans-serif"],
       },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 5s"
+      }
     },
   },
   plugins: [],
